4ab7c431995c71270eb4c25a26d4346b4cd04e21
John Gutierrez <djterez88@gmail.com>
2021-02-28 10:51:33 -0800

    Setting up django templates

--- my_app/views.py
+++ my_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+import re
+#import requests
+#from requests.compat import quote_plus
+#from .models import Search
+from bs4 import BeautifulSoup
+from  bs4.element import Tag
 
+'''
+page = requests.get(
+    'https://forecast.weather.gov/MapClick.php?lat=41.8843&lon=-87.6324#.XIRQYFNKgUE'
+)
+'''
 # Create your views here.
+
